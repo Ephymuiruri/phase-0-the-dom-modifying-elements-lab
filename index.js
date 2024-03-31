3e897cf799b40c901d0b242b46b52095529787d1
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-03-31 09:25:38 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here! //
+const main =document.querySelector ("main#main")
+main.remove()
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id='victory'
+const YOURName= 'Ephy'
+newHeader.textContent=`${YOURName} is the champion`
+
